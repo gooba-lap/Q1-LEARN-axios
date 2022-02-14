b650d7f76f2e87633067e99188d5b578e17b2303
wasinkp <73060136+SinsamutQ@users.noreply.github.com>
2022-02-14 13:32:09 +0700

    axios post to mock api 🏄🏻‍♂️

--- crud-operation/src/component/create/create.js
+++ crud-operation/src/component/create/create.js
@@ -1,25 +1,38 @@
 import React, { useState } from 'react'
 import { Button, Checkbox, Form } from 'semantic-ui-react'
+import axios from 'axios';
 
 export default function Create() {
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
+
+    console.log(firstName);
+    console.log(lastName);
+
+    const sendDataToAPI = () => {
+        axios.post(`https://6209db5f92946600171c554d.mockapi.io/Crud`, {
+            firstName,
+            lastName
+        })
+    }
 
     return (
         <div>
             <Form>
                 <Form.Field>
                     <label>First Name</label>
-                    <input name="fname" placeholder='First Name' />
+                    <input name="fname" 
+                    onChange={(e) => setFirstName(e.target.value)}
+                    placeholder='First Name' />
                 </Form.Field>
                 <Form.Field>
                     <label>Last Name</label>
-                    <input name="lname" placeholder='Last Name' />
+                    <input name="lname" 
+                    onChange={(e) => setLastName(e.target.value)}
+                    placeholder='Last Name' />
                 </Form.Field>
-                <Form.Field>
-                    <Checkbox label='I agree to the Terms and Conditions' />
-                    </Form.Field>
-                <Button type='submit'>Submit</Button>
+                
+                <Button type='submit' onClick={sendDataToAPI}>Submit</Button>
             </Form>
         </div>
     )
